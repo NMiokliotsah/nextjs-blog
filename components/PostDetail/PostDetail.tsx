435244a95bf9f537667f60d3a9db29b8df0c8032
Molosh Nikita <nmiokliotsah@gmail.com>
2022-11-14 17:00:42 +1300

    highlite code section

--- components/PostDetail/PostDetail.tsx
+++ components/PostDetail/PostDetail.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import style from './PostDetail.module.scss';
 
@@ -19,6 +21,19 @@ function PostDetail({ post }) {
         height={300}
       />
     },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    }
   }
 
   return <article className={style.content}>
