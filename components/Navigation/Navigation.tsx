d9d03979e03176d9d3df6aedb300cde0af7e9330
Molosh Nikita <nmiokliotsah@gmail.com>
2022-11-21 15:32:34 +1300

    add logout logic

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import { signOut } from "next-auth/react"
 import Logo from "../Logo/Logo";
 import style from './Navigation.module.scss';
 
 function Navigation() {
+  const handleLogoutButton = () => {
+    signOut();
+  }
+
   return <header className={style.blogHeader}>
     <Link href='/'>
       <Logo />
@@ -11,6 +16,9 @@ function Navigation() {
       <ul>
         <li><Link href='/posts'>Posts</Link></li>
         <li><Link href='/contact'>Contact</Link></li>
+        <li>
+          <button onClick={handleLogoutButton} className={style.logout}>Logout</button>
+        </li>
       </ul>
     </nav>
   </header>
